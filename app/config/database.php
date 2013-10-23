7b944a26a7f5f0c5c6e08f500ffaeaa6c73279ed
James Arnold <jamesarnolduk@gmail.com>
2013-10-23 13:02:21 +0100

    Config updates

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'sacf_trg',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
